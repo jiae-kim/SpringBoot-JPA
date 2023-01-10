e923364eba8d53b205ce6a8f29485888e229b0d2
jiae-kim <kzioh950522@gmail.com>
2023-01-11 00:34:19 +0900

    Create Delivery

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery") // 하나의 배송은 하나의 주문정보
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // ORDINAL 절대 금지
+    private DeliveryStatus status; // READY, COMP
+}
